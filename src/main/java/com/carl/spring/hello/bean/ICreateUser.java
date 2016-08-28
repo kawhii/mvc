34de3ce1717c2d306b5bddf07ddaf0a0c0b655c1
laomazi2006 <756884434@qq.com>
2016-08-28 10:42:55 +0800

    1.add spring

--- src/main/java/com/carl/spring/hello/bean/ICreateUser.java
+++ src/main/java/com/carl/spring/hello/bean/ICreateUser.java
@@ -4,23 +4,5 @@ package com.carl.spring.hello.bean;
  * Created by Carl on 2016/8/27.
  */
 public interface ICreateUser {
-    /**
-     * 名字这样子啊 是的就是这样
-     * @return
-     */
     String getName();
-
-    default String getScope() {
-        return "user";
-    }
-
-    /**
-     * age
-     * @return
-     */
-    default int getAge() {
-        return 0;
-    }
-
-    default void test() {}
 }
