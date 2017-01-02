4a4e40bbb9e16ed6b6a1b34acd3858fe675c86b5
laomazi2006 <756884434@qq.com>
2017-01-02 15:07:12 +0800

    调整测试类

--- src/main/test/com/test51/carl/spring/mongodb/SpringBaseTester.java
+++ src/main/test/com/test51/carl/spring/mongodb/SpringBaseTester.java
@@ -1,14 +1,19 @@
 package com.test51.carl.spring.mongodb;
 
 import org.junit.Before;
+import org.junit.runner.RunWith;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 /**
  * 基础测试类
  * @author Carl
  * @date 2016/10/23
  */
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration("classpath:spring/application-context.xml")
 public abstract class SpringBaseTester {
     private ClassPathXmlApplicationContext applicationContext;
     @Before
