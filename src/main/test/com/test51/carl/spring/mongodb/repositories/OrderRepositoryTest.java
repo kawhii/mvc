6ff3e09e410c00e32f3cf1789553339b7ead5c66
laomazi2006 <756884434@qq.com>
2017-01-02 17:32:44 +0800

    新增查询

--- src/main/test/com/test51/carl/spring/mongodb/repositories/OrderRepositoryTest.java
+++ src/main/test/com/test51/carl/spring/mongodb/repositories/OrderRepositoryTest.java
@@ -2,6 +2,7 @@ package com.test51.carl.spring.mongodb.repositories;
 
 import com.mongodb.Block;
 import com.mongodb.client.AggregateIterable;
+import com.mongodb.client.FindIterable;
 import com.mongodb.client.MongoCollection;
 import com.test51.carl.spring.mongodb.SpringBaseTester;
 import org.bson.Document;
@@ -11,6 +12,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import java.util.Arrays;
 
 import static com.mongodb.client.model.Aggregates.limit;
+import static com.mongodb.client.model.Filters.and;
+import static com.mongodb.client.model.Filters.eq;
+import static com.mongodb.client.model.Filters.gte;
+import static com.mongodb.client.model.Indexes.ascending;
+import static com.mongodb.client.model.Indexes.text;
 import static org.junit.Assert.*;
 
 /**
@@ -44,5 +50,30 @@ public class OrderRepositoryTest extends SpringBaseTester {
             System.out.println(document);
         });
         assertEquals(1, i[0]);
+    }
+
+    @Test
+    public void find1() throws Exception {
+        //db.orders.find({"items.price" : {"$gte" : 5}});
+        FindIterable iterable = collection().find(gte("items.price", 5));
+        iterable.forEach((Block) document ->
+                System.out.println(document)
+        );
+    }
+
+    @Test
+    public void find2() throws Exception {
+        //db.orders.find({"items.price" : {"$gte" : 1},"price" : 230});
+        FindIterable iterable = collection().find(and(gte("items.price", 5), eq("price", 230)));
+        iterable.forEach((Block) document ->
+                System.out.println(document)
+        );
+    }
+
+    @Test
+    public void createIndex() throws Exception {
+        //db.orders.dropIndex({ "cust_id":1})
+        //db.orders.createIndex({ "cust_id":1})
+        collection().createIndex(ascending("cust_id"));
     }
 }
