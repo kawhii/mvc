72be24a1c66d6c09ea99ac5be7afd34b71440600
laomazi2006 <756884434@qq.com>
2017-01-02 16:57:58 +0800

    修改订单数据源

--- src/main/test/com/test51/carl/spring/mongodb/repositories/OrderRepositoryTest.java
+++ src/main/test/com/test51/carl/spring/mongodb/repositories/OrderRepositoryTest.java
@@ -1,9 +1,16 @@
 package com.test51.carl.spring.mongodb.repositories;
 
+import com.mongodb.Block;
+import com.mongodb.client.AggregateIterable;
+import com.mongodb.client.MongoCollection;
 import com.test51.carl.spring.mongodb.SpringBaseTester;
+import org.bson.Document;
 import org.junit.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 
+import java.util.Arrays;
+
+import static com.mongodb.client.model.Aggregates.limit;
 import static org.junit.Assert.*;
 
 /**
@@ -19,4 +26,23 @@ public class OrderRepositoryTest extends SpringBaseTester {
         assertNotNull(orderRepository);
     }
 
+    @Test
+    public void count() throws Exception {
+        assertEquals(2, collection().count());
+    }
+
+    protected MongoCollection<Document> collection() {
+        return orderRepository.getDBCollection();
+    }
+
+    @Test
+    public void limitQuery() throws Exception {
+        AggregateIterable iterable = collection().aggregate(Arrays.asList(limit(1)));
+        final int[] i = {0};
+        iterable.forEach((Block) document -> {
+            i[0]++;
+            System.out.println(document);
+        });
+        assertEquals(1, i[0]);
+    }
 }
