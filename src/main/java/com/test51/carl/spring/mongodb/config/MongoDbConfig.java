3c8913b4410cf425845282851e875632b006cf51
laomazi2006 <756884434@qq.com>
2017-01-03 23:51:16 +0800

    spring-mongo配置类

--- src/main/java/com/test51/carl/spring/mongodb/config/MongoDbConfig.java
+++ src/main/java/com/test51/carl/spring/mongodb/config/MongoDbConfig.java
@@ -5,6 +5,7 @@ import com.mongodb.client.MongoDatabase;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.mongodb.core.MongoTemplate;
 
 import java.net.UnknownHostException;
 
@@ -15,12 +16,18 @@ import java.net.UnknownHostException;
 @Configuration
 public class MongoDbConfig {
     @Bean
-    public MongoClient mongoClient(@Value("${mongodb.host}") String host) throws UnknownHostException {
+    public MongoClient mongoClient(@Value("${mongodb.host}") String host) {
         return new MongoClient(host);
     }
 
     @Bean
     public MongoDatabase db(@Value("${mongodb.db_name}") String dbName, @Value("${mongodb.host}") String host) throws UnknownHostException {
         return mongoClient(host).getDatabase(dbName);
+    }
+
+    @Bean
+    public MongoTemplate mongoTemplate(@Value("${mongodb.host}") String host, @Value("${mongodb.db_name}") String dbName) {
+        MongoTemplate template = new MongoTemplate(mongoClient(host), dbName);
+        return template;
     }
 }
